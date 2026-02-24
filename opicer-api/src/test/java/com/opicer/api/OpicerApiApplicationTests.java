05253ed210207ee80da9c498ffe0fc3a01ec0851
Kyoungwoong <kyy980708@gmail.com>
2026-02-24 17:30:49 +0900

    feat(config): add security baseline and postgres profile

---
+++ opicer-api/src/test/java/com/opicer/api/OpicerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opicer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpicerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
